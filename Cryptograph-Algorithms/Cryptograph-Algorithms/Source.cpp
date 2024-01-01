7f96344aef76bb61a29c2240427b6b36350e3ee6
AbdelrahmanAbdelfattah <abdelrahmanmohamed702413@gmail.com>
2024-01-01 23:10:14 +0200

    Implement Diffie-Hellmen key exchange algorithm using Elliptic curve

--- Cryptograph-Algorithms/Cryptograph-Algorithms/Source.cpp
+++ Cryptograph-Algorithms/Cryptograph-Algorithms/Source.cpp
@@ -1,19 +1,33 @@
 #include "ECC.h"
-
-#include <bitset>
+#include "randomGenerator.h"
 
 
 int main() {
 
-    ECC* ecc = new ECC(0, 7);
+    // these are  all public : the ECC and generator point 
+    ECC* ecc = new ECC(3, 7);
+    Point generator = Point(-2, 1);
 
-    Point generator = Point(1, 1);
+    randomGenerator rd = randomGenerator(10000);
 
-    Point p = ecc->DoubleAndAdd(100, generator);
+    // Elliptic Curve Diffie-Hellmen Algorithm
+    // random number for alice 
+    int a = rd.generateRandomNumber();
+    //random number for bob
+    int b = rd.generateRandomNumber();
 
+    // public keys with double and add algorithm 
+    // these are points on Elliptic curve 
+    Point alicePublicKey = ecc->DoubleAndAdd(a, generator);
+    Point bobPublicKey = ecc->DoubleAndAdd(b, generator);
 
+    //they can generate the same private key  they can use for symmetric encryption
+    Point aliceSecretKey = ecc->DoubleAndAdd(a, bobPublicKey);
+    Point bobSecretKey = ecc->DoubleAndAdd(b, alicePublicKey);
 
-    cout << p.toString();
+    cout << aliceSecretKey.toString() << endl;
+    cout << bobSecretKey.toString() << endl;
+
 
     return 0;
 }
